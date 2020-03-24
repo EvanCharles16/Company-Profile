3c8dc5749bac1e4ab9b40dc3ddadf61ba53d1c31
EvanCharles16 <evancharles1609@gmail.com>
2020-03-24 17:52:08 +0700

    Navbar

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// $('.dropdown-toogle').dropdown({
+//     offset: '0'
+// })
+
+$('.carousel').carousel({
+    indicators: false, //Untuk Titik pada Slideshow
+      height: 200, //Tinggi Slideshow
+      transition: 600, //Transisi Tulisannya (ms)
+      interval: 3000 //Transisi Foto (ms)
+  })
